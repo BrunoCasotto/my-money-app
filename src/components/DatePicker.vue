75741e51fe49bbb3ad538ba46b75569623804232
Bruno Casotto <casottoalves@hotmail.com>
2018-04-08 23:02:38 -0300

    Date picker config done

--- src/components/DatePicker.vue
+++ src/components/DatePicker.vue
@@ -21,24 +21,34 @@ export default {
     }
   },
   mounted() {
-    let elements = document.querySelectorAll('.datepicker');
-    elements.forEach((elem) => {
-      MaterializeJs.Datepicker.init(elem, {
-        format: 'dd mmm, yyyy',
-        i18n: {
-          today: 'Hoje',
-          clear: 'Limpar',
-          done: 'Ok',
-          nextMonth: 'Próximo mês',
-          previousMonth: 'Mês anterior',
-          weekdaysAbbrev: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'],
-          weekdaysShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'],
-          weekdays: ['Domingo', 'Segunda-Feira', 'Terça-Feira', 'Quarta-Feira', 'Quinta-Feira', 'Sexta-Feira', 'Sábado'],
-          monthsShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
-          months: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
-        }
-      });
-    })
+    this.setDatePicker()
+  },
+  methods: {
+    NotifyChange() {
+      console.log("new date")
+    },
+    setDatePicker() {
+      let elements = document.querySelectorAll('.datepicker')
+
+      elements.forEach((elem) => {
+        MaterializeJs.Datepicker.init(elem, {
+          format: 'dd mmm, yyyy',
+          onClose: this.NotifyChange,
+          i18n: {
+            today: 'Hoje',
+            clear: 'Limpar',
+            done: 'Ok',
+            nextMonth: 'Próximo mês',
+            previousMonth: 'Mês anterior',
+            weekdaysAbbrev: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'],
+            weekdaysShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'],
+            weekdays: ['Domingo', 'Segunda-Feira', 'Terça-Feira', 'Quarta-Feira', 'Quinta-Feira', 'Sexta-Feira', 'Sábado'],
+            monthsShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+            months: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+          }
+        })
+      })
+    }
   }
 }
 </script>
